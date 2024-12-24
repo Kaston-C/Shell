198bc0a7af23a9de3236d756c9b6f6b3e45d49a9
Kaston-C <kaston_carr@Mac.lan>
2024-12-24 17:24:04 -0500

    echo command

--- app/main.c
+++ app/main.c
@@ -17,8 +17,13 @@ int main() {
     input[strlen(input) - 1] = '\0';
 
     // if user wants to exit
-    if (!strcmp(input, "exit 0"))
+    if (strncmp(input, "exit 0", 6) == 0) {
       exit(0);
+    } else if (strncmp(input, "echo ", 5) == 0) {
+      printf("%s\n", input + 5);
+    } else {
+      printf("%s: command not found\n", input);
+    }
 
     printf("%s: command not found\n", input);
   }
