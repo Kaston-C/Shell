0f84b62465c5674f33ddd0158764e672dd1a7be1
Kaston-C <kaston_carr@Mac.lan>
2024-12-24 16:59:59 -0500

    exit command

--- app/main.c
+++ app/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 int main() {
   // REPL loop
@@ -14,6 +15,11 @@ int main() {
     fgets(input, 100, stdin);
     // set newline to null terminator
     input[strlen(input) - 1] = '\0';
+
+    // if user wants to exit
+    if (!strcmp(input, "exit 0"))
+      exit(0);
+
     printf("%s: command not found\n", input);
   }
 
