a305a77dca255a55dd76c2d21c70db3fff02de1a
Kaston-C <kaston_carr@Mac.lan>
2024-12-24 16:52:24 -0500

    REPL loop

--- app/main.c
+++ app/main.c
@@ -2,16 +2,20 @@
 #include <string.h>
 
 int main() {
-  // Flush after every printf
-  setbuf(stdout, NULL);
+  // REPL loop
+  while (1) {
+    // Flush after every printf
+    setbuf(stdout, NULL);
 
-  printf("$ ");
+    printf("$ ");
 
-  // Wait for user input
-  char input[100];
-  fgets(input, 100, stdin);
-  // set newline to null terminator
-  input[strlen(input) - 1] = '\0';
-  printf("%s: command not found\n", input);
+    // Wait for user input
+    char input[100];
+    fgets(input, 100, stdin);
+    // set newline to null terminator
+    input[strlen(input) - 1] = '\0';
+    printf("%s: command not found\n", input);
+  }
+
   return 0;
 }
