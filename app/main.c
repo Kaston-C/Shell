11addb05ab7303946ef578b26e0f402c0cd05f8a
Kaston-C <kaston_carr@Mac.lan>
2024-12-26 19:02:22 -0500

    type

--- app/main.c
+++ app/main.c
@@ -16,13 +16,22 @@ int main() {
     // set newline to null terminator
     input[strlen(input) - 1] = '\0';
 
-    // if user wants to exit
     if (strncmp(input, "exit 0", 6) == 0) {
-      exit(0);
+        exit(0);
     } else if (strncmp(input, "echo ", 5) == 0) {
-      printf("%s\n", input + 5);
+        printf("%s\n", input + 5);
+    } else if (strncmp(input, "type ", 5) == 0) {
+        if (strncmp(input + 5, "exit", 4) == 0) {
+            printf("exit is a shell builtin");
+        } else if (strncmp(input + 5, "echo", 4) == 0) {
+            printf("echo is a shell builtin");
+        } else if (strncmp(input + 5, "type", 4) == 0) {
+            printf("type is a shell builtin");
+        } else {
+            printf("%s: not found\n", input + 5);
+        }
     } else {
-      printf("%s: command not found\n", input);
+        printf("%s: command not found\n", input);
     }
   }
 
